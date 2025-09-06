e79afb85526032f262b5e9e775b001728c2c05e7
Abdul Haseeb Windows <a.official.haseeb@gmail.com>
2025-09-06 16:35:49 +0500

    almost final

--- src/store/grid-data.ts
+++ src/store/grid-data.ts
@@ -10,7 +10,7 @@ export default {
         expanded: "ProfileCard",
       },
       styles: {
-        normal: `h-full border rounded-lg col-span-2 lg:row-span-2 flex justify-center items-center ${colorScheme.dark.background} ${colorScheme.light.background}`,
+        normal: `h-full border rounded-lg col-span-2 lg:row-span-2 flex justify-center items-center ${colorScheme.dark.background} ${colorScheme.light.background} ${colorScheme.shadow}`,
         expanded: `w-full col-span-full bg-transparent row-strat-auto `,
       },
     },
@@ -62,7 +62,7 @@ export default {
       },
       styles: {
         normal:
-          "h-48 md:h-56  col-span-1 flex justify-center items-center rounded-lg",
+          `h-48 md:h-56  col-span-1 flex justify-center items-center rounded-lg  ${colorScheme.shadow}`,
         expanded: "h-full col-span-full my-8 rounded-lg",
       },
     },
@@ -75,7 +75,7 @@ export default {
       },
       styles: {
         normal:
-          "h-48 md:h-56  col-span-2 row-start-4 lg:row-start-auto flex justify-center items-center rounded-2xl",
+          `h-48 md:h-56  col-span-2 row-start-4 lg:row-start-auto flex justify-center items-center rounded-2xl  ${colorScheme.shadow}`,
         expanded: "h-full col-span-full row-start-5 rounded-lg my-8",
       },
     },
@@ -88,7 +88,7 @@ export default {
       },
       styles: {
         normal:
-          "h-48 md:h-56 rounded-2xl col-span-1 flex justify-center items-center rounded-lg",
+          `h-48 md:h-56 rounded-2xl col-span-1 flex justify-center items-center rounded-lg  ${colorScheme.shadow}`,
         expanded:
           "h-full col-span-full row-strat-auto lg:row-strat-auto  rounded-lg my-8",
       },
@@ -101,7 +101,7 @@ export default {
         expanded: "StatsCard",
       },
       styles: {
-        normal: `h-full md:h-56 rounded-2xl col-span-2 flex justify-center items-center rounded-lg ${colorScheme.dark.background} ${colorScheme.light.background}`,
+        normal: `h-full md:h-56 rounded-2xl col-span-2 flex justify-center items-center rounded-lg ${colorScheme.dark.background} ${colorScheme.light.background}  ${colorScheme.shadow}`,
         expanded:
           "h-full col-span-full row-start-auto bg-transparent  rounded-lg",
       },
@@ -115,7 +115,7 @@ export default {
       },
       styles: {
         normal:
-          "h-48 md:h-56 rounded-2xl  col-span-2 flex justify-center items-center rounded-lg",
+          `h-48 md:h-56 rounded-2xl  col-span-2 flex justify-center items-center rounded-lg  ${colorScheme.shadow}`,
         expanded: "h-full col-span-full row-start-auto rounded-lg  my-8",
       },
     },
